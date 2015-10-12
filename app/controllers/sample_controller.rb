fc2a89d9d43c70bc9e7c7d5aa9fe1a5d25d5ae0e
Ellery Durgin <ellery@saidigital.co>
2015-10-12 00:08:30 -0400

    added Daylight savings time

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -5,17 +5,18 @@ class SampleController < ApplicationController
 
   def create
     schedule = IceCube::Schedule.new
-    schedule.add_recurrence_rule(RecurringSelect.dirty_hash_to_rule(params[:fake_model][:recurring_rules])  )
-    @record = convert_date(schedule, params[:fake_model][:published_at_time],10)
+    schedule.add_recurrence_rule(RecurringSelect.dirty_hash_to_rule(params[:fake_model][:recurring_rules]) )
+    b = []
+    time =DateTime.parse(params[:fake_model][:published_at_time])
+    schedule.first(10).each do |a| 
+      b << Time.local(a.year, a.month, a.day, time.hour, time.min)
+    end
+    @record = b
+
   end
   
   def convert_date(date,time,count)
-    @a = []
-    time = DateTime.parse(time)
-    date.first(count).each do |a| 
-      @a << Time.local(a.year,a.month ,a.day, time.hour, time.min)
-    end
-    return @a 
+   
   end
 
 end
